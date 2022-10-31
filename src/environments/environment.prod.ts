423f0ebe2098d1e5defcb8d98f7b9bd9f9158b05
JoseRossi19 <jose.rossigarcia@gmail.com>
2022-10-31 18:42:54 -0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'backjgr',
+    appId: '1:617449614413:web:2c8f5582ba89f7c0d00004',
+    storageBucket: 'backjgr.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAT4CAvi2qw_gLNBpqAcRlMZnLOI3-W_Rg',
+    authDomain: 'backjgr.firebaseapp.com',
+    messagingSenderId: '617449614413',
+  },
+  production: true,
+  URL:  'https://backjgr.herokuapp.com/'
+};
