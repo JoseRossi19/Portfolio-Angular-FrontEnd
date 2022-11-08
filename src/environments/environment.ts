63c90d5474456c4eb32b31198bba6e40dffbf6f9
JoseRossi19 <jose.rossigarcia@gmail.com>
2022-11-07 22:45:29 -0300

    act firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,14 @@
 
 export const environment = {
   firebase: {
-    projectId: 'frontendok-fbb8f',
-    appId: '1:200470013161:web:693a629b25986ff1efbc78',
-    storageBucket: 'frontendok-fbb8f.appspot.com',
-    apiKey: 'AIzaSyCj9xGYVzN_IlEsGF5V-hCm2kHO23aC2r8',
-    authDomain: 'frontendok-fbb8f.firebaseapp.com',
-    messagingSenderId: '200470013161',
+    projectId: 'okfront711',
+    appId: '1:173044464574:web:dfc4c2267ee6b3cdfed7ef',
+    databaseURL: 'https://okfront711-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'okfront711.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBAcxRpWPP1XW5PCgASc9-RVrqdC7MeMwY',
+    authDomain: 'okfront711.firebaseapp.com',
+    messagingSenderId: '173044464574',
   },
   production: false,
 
