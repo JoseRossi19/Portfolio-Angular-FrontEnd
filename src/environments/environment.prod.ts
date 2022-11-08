fd0451c25d25584ee5fc3e6d19b6548142d5a650
JoseRossi19 <jose.rossigarcia@gmail.com>
2022-11-07 22:55:17 -0300

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   firebase: {
     projectId: 'okfront711',
     appId: '1:173044464574:web:dfc4c2267ee6b3cdfed7ef',
-    databaseURL: 'https://okfront711-default-rtdb.asia-southeast1.firebasedatabase.app',
     storageBucket: 'okfront711.appspot.com',
     locationId: 'southamerica-east1',
     apiKey: 'AIzaSyBAcxRpWPP1XW5PCgASc9-RVrqdC7MeMwY',
